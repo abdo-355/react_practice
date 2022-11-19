22e545519b6aec38f025f0fd605852803c180d22
abdo-355 <abdoluxor366@gmail.com>
2022-11-19 23:13:15 +0200

    add styling to the ExpenseItem component

--- expenses_app/src/components/ExpenseItem.tsx
+++ expenses_app/src/components/ExpenseItem.tsx
@@ -1,12 +1,14 @@
+import "./ExpenceItem.css";
+
 const ExpenseItem: React.FC = () => {
   return (
-    <>
+    <div className="expense-item">
       <div>November 19th 2022</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Internet subscription</h2>
-        <div>$14,95</div>
+        <div className="expense-item__price">$14,95</div>
       </div>
-    </>
+    </div>
   );
 };
 
