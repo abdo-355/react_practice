a9d43f219847c857d247bb7ca69169510ded5126
abdo-355 <abdoluxor366@gmail.com>
2022-11-21 13:48:00 +0200

    add Expeses filter component with its styles and functionality

--- expenses_app/src/components/Expenses/Expenses.tsx
+++ expenses_app/src/components/Expenses/Expenses.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+
 import { IExpenseItem } from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 
 import "./Expenses.css";
 
@@ -9,8 +12,18 @@ interface Props {
 }
 
 const Expenses: React.FC<Props> = ({ expenses }) => {
+  const [filteredYear, setFilteredYear] = useState("2022");
+
+  const filterChangeHandler = (selectedYear: string) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       {expenses.map((expense) => (
         <ExpenseItem key={expense.id} {...expense} />
       ))}
