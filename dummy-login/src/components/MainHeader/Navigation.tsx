6ab9a0cf989b4e48d3c10fe54ac85ed32f301ca0
abdo-355 <abdoluxor366@gmail.com>
2022-11-29 22:41:07 +0200

    replacing the consumer component with useContext hook for more elegent code

--- dummy-login/src/components/MainHeader/Navigation.tsx
+++ dummy-login/src/components/MainHeader/Navigation.tsx
@@ -1,33 +1,30 @@
+import { useContext } from "react";
 import AuthContext from "../../context/auth-context";
 import classes from "./Navigation.module.css";
 
 const Navigation = () => {
+  const { isLoggedIn, onLogout } = useContext(AuthContext);
+
   return (
-    <AuthContext.Consumer>
-      {({ isLoggedIn, onLogout }) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {isLoggedIn && (
-                <li>
-                  <button onClick={onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {isLoggedIn && (
+          <li>
+            <button onClick={onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
