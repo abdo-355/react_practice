719355fd351140fa8d8693e6f0d45f10b17f576d
abdo-355 <abdoluxor366@gmail.com>
2022-12-08 23:48:05 +0200

    add error handling

--- movies_app/src/App.tsx
+++ movies_app/src/App.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, useState } from "react";
+import { MouseEvent, ReactNode, useState } from "react";
 
 import "./App.module.css";
 import MoviesList from "./components/MoviesList";
@@ -7,11 +7,16 @@ import { IMovie } from "./types/types";
 const App = () => {
   const [movies, setMovies] = useState<IMovie[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
 
   const onClickHandler = async (e: MouseEvent<HTMLButtonElement>) => {
     try {
       setIsLoading(true);
-      const response = await fetch("https://swapi.dev/api/films");
+      const response = await fetch("https://swapi.dev/api/film");
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
 
       const data = await response.json();
 
@@ -26,21 +31,36 @@ const App = () => {
 
       setMovies(transformedData);
       setIsLoading(false);
-    } catch (err) {
-      console.log(err);
+    } catch (err: any) {
+      setError(err);
+      setIsLoading(false);
     }
   };
+
+  let content: ReactNode = <p>"No Movies found"</p>;
+
+  if (movies.length === 0) {
+    content = <p>No movies found</p>;
+  }
+
+  if (error) {
+    content = <p>{error.message}</p>;
+  }
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
 
   return (
     <>
       <section>
         <button onClick={onClickHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>No movies found</p>}
-        {isLoading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </>
   );
 };
