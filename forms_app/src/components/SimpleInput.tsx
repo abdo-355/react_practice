7776c726e6fd45f5ff5c851e681fd12eff9486ca
abdo-355 <abdoluxor366@gmail.com>
2022-12-11 19:48:49 +0200

    add validation to the name input field

--- forms_app/src/components/SimpleInput.tsx
+++ forms_app/src/components/SimpleInput.tsx
@@ -2,15 +2,18 @@ import { useState, ChangeEvent, FormEvent } from "react";
 
 const SimpleInput = () => {
   const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
 
   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
     setEnteredName(e.target.value);
+    setEnteredNameIsValid(true);
   };
 
   const onSubmitHandler = (e: FormEvent) => {
     e.preventDefault();
 
     if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
       return;
     }
 
@@ -19,9 +22,13 @@ const SimpleInput = () => {
     setEnteredName("");
   };
 
+  const nameInputClasses = enteredNameIsValid
+    ? "form-control"
+    : "form-control invalid";
+
   return (
     <form onSubmit={onSubmitHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           type="text"
@@ -29,6 +36,9 @@ const SimpleInput = () => {
           onChange={onChangeHandler}
           value={enteredName}
         />
+        {!enteredNameIsValid && (
+          <p className="error-text">Name must not be Empty</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
