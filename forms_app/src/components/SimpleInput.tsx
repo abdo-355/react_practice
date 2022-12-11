e3c727e6ddfd2985844cea7b3ff88ce26f94fe4c
abdo-355 <abdoluxor366@gmail.com>
2022-12-11 20:51:51 +0200

    enhance the validation logic by implementing onBlur and with every keystroke validation

--- forms_app/src/components/SimpleInput.tsx
+++ forms_app/src/components/SimpleInput.tsx
@@ -1,16 +1,28 @@
-import { useState, ChangeEvent, FormEvent } from "react";
+import { useState, ChangeEvent, FormEvent, FocusEvent } from "react";
 
 const SimpleInput = () => {
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [nameInputTouched, setNameInputTouched] = useState(false);
 
   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
     setEnteredName(e.target.value);
     setEnteredNameIsValid(true);
   };
 
+  const onBlurHandler = (e: FocusEvent<HTMLInputElement>) => {
+    setNameInputTouched(true);
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+  };
+
   const onSubmitHandler = (e: FormEvent) => {
     e.preventDefault();
+
+    setNameInputTouched(true);
 
     if (enteredName.trim() === "") {
       setEnteredNameIsValid(false);
@@ -20,11 +32,13 @@ const SimpleInput = () => {
     console.log(enteredName);
 
     setEnteredName("");
+    setNameInputTouched(false);
   };
 
-  const nameInputClasses = enteredNameIsValid
-    ? "form-control"
-    : "form-control invalid";
+  const nameInputClasses =
+    !enteredNameIsValid && nameInputTouched
+      ? "form-control invalid"
+      : "form-control";
 
   return (
     <form onSubmit={onSubmitHandler}>
@@ -34,9 +48,10 @@ const SimpleInput = () => {
           type="text"
           id="name"
           onChange={onChangeHandler}
+          onBlur={onBlurHandler}
           value={enteredName}
         />
-        {!enteredNameIsValid && (
+        {!enteredNameIsValid && nameInputTouched && (
           <p className="error-text">Name must not be Empty</p>
         )}
       </div>
