21579edce82d90f1cb89f8f11b40302836962ca1
abdo-355 <abdoluxor366@gmail.com>
2022-12-11 19:30:27 +0200

    first steps into forms validation

--- forms_app/src/components/SimpleInput.tsx
+++ forms_app/src/components/SimpleInput.tsx
@@ -1,9 +1,34 @@
+import { useState, ChangeEvent, FormEvent } from "react";
+
 const SimpleInput = () => {
+  const [enteredName, setEnteredName] = useState("");
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setEnteredName(e.target.value);
+  };
+
+  const onSubmitHandler = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (enteredName.trim() === "") {
+      return;
+    }
+
+    console.log(enteredName);
+
+    setEnteredName("");
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmitHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input
+          type="text"
+          id="name"
+          onChange={onChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
