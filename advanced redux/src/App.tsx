cf5283d70b8107b2b44410653f0ea205365daf94
abdo-355 <abdoluxor366@gmail.com>
2022-12-21 20:55:07 +0200

    use the cart toggler to show/hide the cart

--- advanced redux/src/App.tsx
+++ advanced redux/src/App.tsx
@@ -1,11 +1,18 @@
+import { useSelector } from "react-redux";
+
 import Cart from "./components/Cart";
 import Layout from "./components/Layout";
 import Products from "./components/Shop/Products";
+import { RootState } from "./store";
 
 const App = () => {
+  const cartIsVisible = useSelector(
+    (state: RootState) => state.ui.cartIsVisible
+  );
+
   return (
     <Layout>
-      <Cart />
+      {cartIsVisible && <Cart />}
       <Products />
     </Layout>
   );
--- advanced redux/src/components/Cart/CartButton.tsx
+++ advanced redux/src/components/Cart/CartButton.tsx
@@ -1,8 +1,17 @@
+import { useDispatch } from "react-redux/es/exports";
+
+import { toggleCart } from "../../store/slices/ui-slice";
 import styles from "./CartButton.module.css";
 
 const CartButton = () => {
+  const dispatch = useDispatch();
+
+  const toggleCartHandler = () => {
+    dispatch(toggleCart());
+  };
+
   return (
-    <button className={styles.button}>
+    <button className={styles.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={styles.badge}>1</span>
     </button>
