cde87324cf4a0cce04db725264ee12c39b37ba0a
abdo-355 <abdoluxor366@gmail.com>
2022-12-21 20:56:36 +0200

    add cart slice

---
+++ advanced redux/src/store/slices/cart-slice.ts
@@ -0,0 +1,63 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface ICartItem {
+  id: string;
+  title: string;
+  price: number;
+  amount: number;
+  total: number;
+}
+
+interface INewItem {
+  id: string;
+  title: string;
+  price: number;
+  description: string;
+}
+
+const cartInitialState = {
+  items: [] as ICartItem[],
+  totalQuantity: 0,
+};
+
+const CartSlice = createSlice({
+  name: "cart",
+  initialState: cartInitialState,
+  reducers: {
+    addItem: (state, action: PayloadAction<INewItem>) => {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          amount: 1,
+          price: newItem.price,
+          title: newItem.title,
+          total: newItem.price,
+        });
+      } else {
+        existingItem.amount++;
+        existingItem.total += newItem.price;
+      }
+    },
+
+    removeItem: (state, action: PayloadAction<string>) => {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+
+      if (!existingItem) {
+        return;
+      } else if (existingItem.amount === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.amount--;
+        existingItem.total -= existingItem.price;
+      }
+    },
+  },
+});
+
+export const { addItem, removeItem } = CartSlice.actions;
+
+export default CartSlice.reducer;
