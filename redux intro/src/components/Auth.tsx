c96f8345fe92558755bf602f52404e89bd41ac83
abdo-355 <abdoluxor366@gmail.com>
2022-12-21 19:11:57 +0200

    use the auth actions in the auth and header to change the authentication state

--- redux intro/src/components/Auth.tsx
+++ redux intro/src/components/Auth.tsx
@@ -1,10 +1,22 @@
+import { FormEventHandler } from "react";
+import { useDispatch } from "react-redux";
+
+import { login } from "../store/slices/authSlice";
 import styles from "./Auth.module.css";
 
 const Auth = () => {
+  const dispatch = useDispatch();
+
+  const submitHandler: FormEventHandler = (e) => {
+    e.preventDefault();
+
+    dispatch(login());
+  };
+
   return (
     <main className={styles.auth}>
       <section>
-        <form>
+        <form onSubmit={submitHandler}>
           <div className={styles.control}>
             <label htmlFor="email">Email</label>
             <input type="email" id="email" />
@@ -13,7 +25,7 @@ const Auth = () => {
             <label htmlFor="password">Password</label>
             <input type="password" id="password" />
           </div>
-          <button>Login</button>
+          <button type="submit">Login</button>
         </form>
       </section>
     </main>
