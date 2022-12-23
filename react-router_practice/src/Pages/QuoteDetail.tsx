1f773f10835466ce2401ce6df6bcf329c04b60d7
abdo-355 <abdoluxor366@gmail.com>
2022-12-23 17:01:57 +0200

    add the comments component in the quoteDetials component

--- react-router_practice/src/Pages/QuoteDetail.tsx
+++ react-router_practice/src/Pages/QuoteDetail.tsx
@@ -1,5 +1,19 @@
+import { useParams, Route } from "react-router-dom";
+
+import Comments from "../components/Comments/Comments";
+
 const QuoteDetails = () => {
-  return <h1>quote details page</h1>;
+  const params = useParams<{ quoteId: string }>();
+
+  return (
+    <>
+      <h1>quote details page</h1>
+      <p>{params.quoteId}</p>
+      <Route path="/quotes/:quoteId/comments">
+        <Comments />
+      </Route>
+    </>
+  );
 };
 
 export default QuoteDetails;
