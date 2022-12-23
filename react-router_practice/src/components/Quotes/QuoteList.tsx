20378e5220bc2e934344abb9851d9ac6adae1561
abdo-355 <abdoluxor366@gmail.com>
2022-12-23 20:51:10 +0200

    add sorting functionality

--- react-router_practice/src/components/Quotes/QuoteList.tsx
+++ react-router_practice/src/components/Quotes/QuoteList.tsx
@@ -1,3 +1,5 @@
+import { useHistory, useLocation } from "react-router-dom";
+
 import QuoteItem from "./QuoteItem";
 import styles from "./QuoteList.module.css";
 import { IQuote } from "../../types/types";
@@ -10,11 +12,38 @@ interface Props {
   quotes: Quote[];
 }
 
+const sortQuotes = (quotes: Quote[], ascending: boolean) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList: React.FC<Props> = ({ quotes }) => {
+  const history = useHistory();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+
+  const isAscending = queryParams.get("sort") === "asc";
+
+  const sortedQuotes = sortQuotes(quotes, isAscending);
+
+  const changeSortingMode = () => {
+    history.push("/quotes?sort=" + (isAscending ? "desc" : "asc"));
+  };
+
   return (
     <>
+      <div className={styles.sorting}>
+        <button onClick={changeSortingMode}>
+          Sort {isAscending ? "Descending" : "Ascending"}
+        </button>
+      </div>
       <ul className={styles.list}>
-        {quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
