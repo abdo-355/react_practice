48c8b7d89798ed3dc67f2d77c9bec071299ae8e0
abdo-355 <abdoluxor366@gmail.com>
2022-12-23 18:14:56 +0200

    implement programatic navigation on the form submit

--- react-router_practice/src/Pages/NewQuote.tsx
+++ react-router_practice/src/Pages/NewQuote.tsx
@@ -1,9 +1,15 @@
+import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../components/Quotes/QuoteForm";
 import { IQuote } from "../types/types";
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quote: IQuote) => {
     console.log(quote);
+
+    history.push("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} isLoading={false} />;
