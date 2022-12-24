5c3fab5d7a185ec26da6b5e4a0d0ab46001523a3
abdo-355 <abdoluxor366@gmail.com>
2022-12-25 00:04:30 +0200

    add setting new password functionality

--- react-auth/src/components/Profile/ProfileForm.tsx
+++ react-auth/src/components/Profile/ProfileForm.tsx
@@ -1,11 +1,58 @@
+import { useRef, useContext, FormEventHandler } from "react";
+
+import AuthContext from "../../store/auth-context";
 import styles from "./ProfileForm.module.css";
 
 const ProfileForm = () => {
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const { token } = useContext(AuthContext);
+
+  const submitHandler: FormEventHandler = async (e) => {
+    e.preventDefault();
+
+    const newPassword = passwordRef.current!.value;
+
+    // validation goes here
+
+    const res = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${process.env.REACT_APP_FIREBASE_KEY}`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: token,
+          password: newPassword,
+          returnSecureToken: false,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (res.ok) {
+      // TODO: use the returned data
+    } else {
+      const data = await res.json();
+      let errorMessage = "updating the password failed!";
+
+      if (data && data.error && data.error.message) {
+        errorMessage = data.error.message;
+      }
+
+      alert(errorMessage);
+    }
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <div className={styles.control}>
         <label htmlFor="new-password">New Password</label>
-        <input type="password" id="new-password" />
+        <input
+          ref={passwordRef}
+          type="password"
+          minLength={7}
+          id="new-password"
+        />
       </div>
       <div className={styles.action}>
         <button>Change Password</button>
