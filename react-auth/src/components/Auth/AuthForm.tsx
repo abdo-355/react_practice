021a29e5228be49a958996b528a3e5dcc19ba785
abdo-355 <abdoluxor366@gmail.com>
2022-12-24 22:13:04 +0200

    sign up users with firebase

--- react-auth/src/components/Auth/AuthForm.tsx
+++ react-auth/src/components/Auth/AuthForm.tsx
@@ -1,28 +1,67 @@
-import { useState } from "react";
+import { FormEventHandler, useState, useRef } from "react";
 
 import styles from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
 
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler: FormEventHandler = async (e) => {
+    e.preventDefault();
+
+    const enteredEmail = emailRef.current!.value;
+    const enteredPassword = passwordRef.current!.value;
+
+    // validation goes here
+
+    if (isLogin) {
+      // login
+    } else {
+      const res = await fetch(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_KEY}`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (res.ok) {
+        // TODO: use the returned data
+      } else {
+        const data = await res.json();
+        // TODO: some error handling
+        console.log(data);
+      }
+    }
   };
 
   return (
     <section className={styles.auth}>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={styles.control}>
           <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required />
+          <input ref={emailRef} type="email" id="email" required />
         </div>
         <div className={styles.control}>
           <label htmlFor="password">Your Password</label>
-          <input type="password" id="password" required />
+          <input ref={passwordRef} type="password" id="password" required />
         </div>
         <div className={styles.actions}>
-          <button>{isLogin ? "Login" : "Create Account"}</button>
+          <button type="submit">{isLogin ? "Login" : "Create Account"}</button>
           <button
             type="button"
             className={styles.toggle}
